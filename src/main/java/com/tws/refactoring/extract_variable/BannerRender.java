4794179287f9c88dcd50905d9975e0871eb1a8fb
Kong Fanyue <fykong@thoughtworks.com>
2019-07-03 17:18:03 +0800

    Extract method and variable

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -2,10 +2,17 @@ package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
     String renderBanner(String platform, String browser) {
-        if ((platform.toUpperCase().indexOf("MAC") > -1) &&
-                (browser.toUpperCase().indexOf("IE") > -1)) {
+        if (containMac(platform) && containIE(browser)) {
             return "IE on Mac?";
         }
         return "banner";
+    }
+
+    private boolean containIE(String browser) {
+        return browser.toUpperCase().contains("IE");
+    }
+
+    private boolean containMac(String platform) {
+        return platform.toUpperCase().contains("MAC");
     }
 }
--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -1,10 +1,17 @@
 package com.tws.refactoring.extract_variable;
 
 public class PriceCalculator {
+
+    private static final double DISCOUNT_RATE = 0.05;
+    private static final int DISCOUNT_START = 500;
+    private static final double LOWEST_COST = 100.0;
+    private static final double SHIPPING_PRICE = 0.1;
+
     double getPrice(int quantity, int itemPrice) {
-        // Price consists of: base price - discount + shipping cost
-        return quantity * itemPrice -
-                Math.max(0, quantity - 500) * itemPrice * 0.05 +
-                Math.min(quantity * itemPrice * 0.1, 100.0);
+        int basePrice = quantity * itemPrice;
+        double discount = Math.max(0, quantity - DISCOUNT_START) * itemPrice * DISCOUNT_RATE;
+        double shippingCost = Math.min(quantity * itemPrice * SHIPPING_PRICE, LOWEST_COST);
+
+        return basePrice - discount + shippingCost;
     }
 }
